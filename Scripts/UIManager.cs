a1edca021a35a53e70fa58a3905c50ecd45b6d09
AlexanderBilz <alexander-bilz@t-online.de>
2015-10-25 15:14:32 -0800

    Initial Commit
    Includes the scripts for the custom editor, the scripts for the on the
    fly generator as well as the scripts to control the player.

---
+++ Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void btn_restart()
+	{
+		Application.LoadLevel("EndlessRunnerAssetScene");
+	}
+	public void btn_pause()
+	{
+		if(Time.timeScale != 0){
+			Time.timeScale = 0;
+		}
+		else{
+			Time.timeScale = 1;
+		}
+	}
+}
